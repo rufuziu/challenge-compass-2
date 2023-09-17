6da8c52471fb1060d66f3741e248a9be74642d58
Iury de Mattos <iurydemattos@gmail.com>
2023-09-17 18:25:31 -0300

    feat: initial commit with application.yml(DEV,PROD)

---
+++ auth-and-auto/src/test/java/br/com/ms/authandauto/AuthAndAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ms.authandauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAndAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
