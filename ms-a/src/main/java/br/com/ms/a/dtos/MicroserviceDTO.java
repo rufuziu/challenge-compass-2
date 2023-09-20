bc8d3bf40e6701e2651b6f240962320070fbde84
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-09-20 13:16:49 -0300

    feat: added MicroserviceDTO

---
+++ ms-a/src/main/java/br/com/ms/a/dtos/MicroserviceDTO.java
@@ -0,0 +1,39 @@
+package br.com.ms.a.dtos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MicroserviceDTO {
+  private Long id;
+  private String name;
+
+  private List<UserWithRoleDTO> users;
+
+  public MicroserviceDTO() {
+    users = new ArrayList<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public List<UserWithRoleDTO> getUsers() {
+    return users;
+  }
+
+  public void setUsers(List<UserWithRoleDTO> users) {
+    this.users = users;
+  }
+}
