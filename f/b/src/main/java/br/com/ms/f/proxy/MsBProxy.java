1080761d314f92286963070a2ca580f58f6773f2
barbosa <ns5933988@gmail.com>
2023-09-20 16:51:03 -0300

    feat: added interface MsAProxy

--- f/b/src/main/java/br/com/ms/f/proxy/MsBProxy.java
+++ f/b/src/main/java/br/com/ms/f/proxy/MsBProxy.java
@@ -1,13 +1,20 @@
 package br.com.ms.f.proxy;
 
-import br.com.ms.f.dtos.UserDto;
+import br.com.ms.f.dtos.MicroserviceDto;
+import br.com.ms.f.dtos.output.UserWithMicroservicesAndRolesOutDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="ms-b",url="localhost:8080")
-public interface MsBProxy {
+@FeignClient(name = "ms-f", url = "localhost:8080")
+public interface MsAProxy {
   @GetMapping("/api/users")
-  public List<UserDto> listAllUsers();
+  public List<UserWithMicroservicesAndRolesOutDTO>
+  getAllUsersWithMicroservicesAndRoles();
+
+  @GetMapping("/api/users/microservice/{id-microservice}")
+  public MicroserviceDTO getMicroserviceAndUsers(
+          @PathVariable("id-microservice") Long id);
 }
